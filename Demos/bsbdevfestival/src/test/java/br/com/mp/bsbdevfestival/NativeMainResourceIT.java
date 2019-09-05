f3a02bc2c84c9bee8cf37127f10c5e56760e6572
Marcus Paulo <mpaulobr@gmail.com>
2019-09-04 23:06:09 -0300

    Apresentação do BSB Dev Festival

---
+++ Demos/bsbdevfestival/src/test/java/br/com/mp/bsbdevfestival/NativeMainResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.mp.bsbdevfestival;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeMainResourceIT extends MainResourceTest {
+
+    // Execute the same tests but in native mode.
+}
